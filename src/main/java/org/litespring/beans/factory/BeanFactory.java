04acc1297d041e560875d4cf434164f1eddf8544
邱竣泽 <qiujunze@qiujunzedeMacBook-Pro.local>
2018-07-31 09:35:12 +0800

    init

--- src/main/java/org/litespring/beans/factory/BeanFactory.java
+++
@@ -1,7 +0,0 @@
-package org.litespring.beans.factory;
-
-public interface BeanFactory {
-
-	Object getBean(String beanID);
-
-}
--- src/main/java/org/litespring/beans/factory/config/SingletonBeanRegistry.java
+++
@@ -1,8 +0,0 @@
-package org.litespring.beans.factory.config;
-
-public interface SingletonBeanRegistry {
-	
-	void registerSingleton(String beanName, Object singletonObject);
-	
-	Object getSingleton(String beanName);
-}
